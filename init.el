d6456dc4b812df8090825610175adadd4640bd4e
n.rukin <n.rukin@gmail.com>
2023-01-20 09:27:58 +0300

    org-mode activation

--- init.el
+++ init.el
@@ -3,4 +3,11 @@
 ;; created 20.01.2023
 ;;
 
+;; input method
 (setq default-input-method "russian-computer")
+
+;; org-mode activation
+;; https://orgmode.org/manual/Activation.html
+(global-set-key (kbd "C-c l") #'org-store-link)
+(global-set-key (kbd "C-c a") #'org-agenda)
+(global-set-key (kbd "C-c c") #'org-capture)
