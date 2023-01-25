738298640f5cba8d5a4a025e1160c8ab822d51d3
Никита Рукин <n.rukin@gmail.com>
2023-01-25 12:43:41 +0300

    mastodon show emojis

--- init.el
+++ init.el
@@ -300,7 +300,8 @@
   :config
   (mastodon-discover)
   (mastodon-toot--enable-custom-emoji)
-  (setq mastodon-tl--show-avatars t))
+  (setq mastodon-tl--show-avatars t)
+  (setq mastodon-toot--enable-custom-instance-emoji t))
 
 (use-package lingva
   :if load-mastodon
