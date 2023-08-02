2f3bc81839312e75a50aa2eca1d6fd4eff0983d6
Рукин Никита <n.rukin@gmail.com>
2023-08-02 17:10:38 +0300

    Удалены комментарии

--- init.el
+++ init.el
@@ -2,10 +2,3 @@
 
 (require 'org)
 (org-babel-load-file (expand-file-name "config.org" user-emacs-directory))
-;; (let ((file-name-handler-alist nil))
-;;   ;; If config is pre-compiled, then load that
-;;   (if (file-exists-p (expand-file-name "readme.elc" user-emacs-directory))
-;;       (load-file (expand-file-name "readme.elc" user-emacs-directory))
-;;     ;; Otherwise use org-babel to tangle and load the configuration
-;;     (require 'org)
-;;     (org-babel-load-file (expand-file-name "readme.org" user-emacs-directory))))
