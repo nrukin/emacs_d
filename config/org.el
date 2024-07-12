d198a3ef24b9f33ab49681823e213c59b7ccf18e
Рукин_Н_А <n.rukin@petrovich.tech>
2024-07-12 09:46:57 +0300

    Настроены предопределенные effort-estimate

--- config/org.el
+++ config/org.el
@@ -39,6 +39,8 @@
   (setq org-priority-lowest 69)
   (setq org-clock-out-remove-zero-time-clocks t)
   (setq org-clock-report-include-clocking-task t)
+  (setq org-global-properties
+	'(("Effort_ALL" . "0 0:10 0:30 1:00 2:00 3:00 4:00 5:00 6:00 7:00")))
   (org-babel-do-load-languages
    'org-babel-load-languages
    '((shell . t)))
