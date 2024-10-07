d9ef30a1f5834dffcbb0819047616c547472f2b0
Nikita Rukin <nikita-rukin@yandex.ru>
2024-10-07 20:49:03 +0300

    yasnippets

--- config/prog.el
+++ config/prog.el
@@ -33,9 +33,9 @@
   :ensure t
   :config (yas-global-mode 1))
 
-;; (use-package yasnippet-snippets
-;;   :ensure t
-;;   :after yasnippet)
+(use-package yasnippet-snippets
+  :ensure t
+  :after yasnippet)
 
 (use-package eglot
   :ensure nil
