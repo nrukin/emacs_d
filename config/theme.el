8737886f82e8b4625818bdc384fe6fd1104288b9
Nikita Rukin <nikita-rukin@yandex.ru>
2024-11-12 09:46:53 +0300

    modus operandi theme

--- config/theme.el
+++ config/theme.el
@@ -1,4 +1,7 @@
-(use-package cyberpunk-theme
+;; (use-package cyberpunk-theme
+;;   :ensure t
+;;   :config
+;;   (load-theme 'cyberpunk t))
+(use-package modus-themes
   :ensure t
-  :config
-  (load-theme 'cyberpunk t))
+  :config (load-theme 'modus-operandi t))
