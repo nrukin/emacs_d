c5fe92de40604b8651b0f1681b81284453050db4
Рукин_Н_А <n.rukin@petrovich.tech>
2024-11-20 10:01:46 +0300

    И иконки тоже не нужны
    нужно суровее, т.е. без них

--- init.el
+++ init.el
@@ -16,7 +16,7 @@
  init-files
  '("config/windows-shell-encoding" ".secret/init" "config/packages"
    "config/emacs" "config/elfeed" "config/org" "config/hydra"
-   "config/icons" "config/discover" "config/emms" "config/theme" "config/magit"
+   "config/discover" "config/emms" "config/theme" "config/magit"
    "config/prog" "config/emoji" "config/mastodon" "config/dashboard" ".secret/config"))
 
 (dolist
