57e52dca131ad6d0e677e53690542c38d1ccc6b1
Рукин_Н_А <n.rukin@petrovich.tech>
2025-05-29 15:59:24 +0300

    Возвращаюсь на тему Zenburn

--- config/theme.el
+++ config/theme.el
@@ -1,8 +1,13 @@
-(defvar use-dark-theme nil "Использовать темную тему")
+;; (defvar use-dark-theme nil "Использовать темную тему")
 
-(use-package modus-themes
+;; (use-package modus-themes
+;;   :ensure t
+;;   :config
+;;   (if use-dark-theme
+;;       (load-theme 'modus-vivendi t)
+;;     (load-theme 'modus-operandi t)))
+
+(use-package zenburn-theme
   :ensure t
   :config
-  (if use-dark-theme
-      (load-theme 'modus-vivendi t)
-    (load-theme 'modus-operandi t)))
+  (load-theme 'zenburn t))
